bff1d6d7052953ca8b89b432111426c0fc530a4e
q.serda <quentinserda@gmail.com>
2022-12-29 18:18:16 +0100

    implement Express & pug  and routing our pages

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 app.get('/', (req, res) => {
-  res.render('')
+  res.render('pages/home')
+})
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+app.get('/collections', (req, res) => {
+  res.render('pages/collections')
+})
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail')
 })
 
 app.listen(port, () => {
