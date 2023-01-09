0db5f9bbe4726acf91efa697a4699561b7786654
q.serda <quentinserda@gmail.com>
2023-01-09 22:45:03 +0100

    link prismic API to our application

--- app.js
+++ app.js
@@ -1,23 +1,134 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path')
+require('dotenv').config();
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-app.get('/', (req, res) => {
-  res.render('pages/home')
-})
-app.get('/about', (req, res) => {
-  res.render('pages/about')
-})
-app.get('/collections', (req, res) => {
-  res.render('pages/collections')
-})
-app.get('/detail/:id', (req, res) => {
-  res.render('pages/detail')
-})
+const fetch = ()=>import('node-fetch')
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const Prismic = require('@prismicio/client');
+const PrismicH = require('@prismicio/helpers');
+
+// Initialize the prismic.io api
+const initApi = (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+    fetch,
+  });
+};
+
+// Link Resolver
+const HandleLinkResolver = (doc) => {
+  // Define the url depending on the document type
+  //   if (doc.type === 'page') {
+  //     return '/page/' + doc.uid;
+  //   } else if (doc.type === 'blog_post') {
+  //     return '/blog/' + doc.uid;
+  //   }
+
+  // Default to homepage
+  return '/';
+};
+
+// Middleware to inject prismic context
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: HandleLinkResolver,
+  };
+  res.locals.PrismicH = PrismicH;
+
+  next();
+});
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.locals.basedir = app.get('views');
+
+const handleRequest = async (api) => {
+  const [ home, about, { results: collections }] = await Promise.all([
+    api.getSingle('home'),
+    api.getSingle('about'),
+    api.query(Prismic.predicate.at('document.type', 'collection'), {
+      fetchLinks: 'product.image',
+    }),
+  ]);
+
+  const assets = [];
+
+  /*home.data.gallery.forEach((item) => {
+    assets.push(item.image.url);
+  });
+
+  about.data.gallery.forEach((item) => {
+    assets.push(item.image.url);
+  });
+
+  about.data.body.forEach((section) => {
+    if (section.slice_type === 'gallery') {
+      section.items.forEach((item) => {
+        assets.push(item.image.url);
+      });
+    }
+  });
+*/
+  collections.forEach((collection) => {
+    collection.data.products.forEach((item) => {
+      assets.push(item.products_product.data.image.url);
+    });
+  });
+
+  return {
+    home,
+    collections,
+    about,
+  };
+};
+
+app.get('/', async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+
+  res.render('pages/home', {
+    ...defaults,
+  });
+});
+
+app.get('/about', async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+
+  res.render('pages/about', {
+    ...defaults,
+  });
+});
+
+app.get('/collection', async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+
+  res.render('pages/collection', {
+    ...defaults,
+  });
+});
+
+app.get('/detail/:uid', async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+
+  const product = await api.getByUID('product', req.params.uid, {
+    fetchLinks: 'collection.title',
+  });
+
+  res.render('pages/detail', {
+    ...defaults,
+    product,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
